720321d097b9ddd5626baf53eef996a9bf8c2ca6
Tom Westerhout <t.westerhout@student.ru.nl>
2018-02-19 01:40:13 +0100

    Considerably simplify the interface: drop pipes in favour of simple lists.

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE BangPatterns #-}
 
 -- |
@@ -10,196 +8,144 @@
 -- Maintainer  : t.westerhout@student.ru.nl
 -- Stability   : experimental
 module Lexer
-  ( Token(..)
-  , scan
-  , scanSimple
-  , textStream
+  ( scanSimple
   , main
   ) where
 
-import Control.DeepSeq
-import Control.Monad
+-- import Control.DeepSeq
+-- import Control.Monad
 import Control.Monad.Trans.Except
 import qualified Data.Char as Char
-import Data.Functor.Identity
+
+-- import Data.Functor.Identity
 import Data.Semigroup
 import qualified Data.Text as T
-import GHC.Generics (Generic)
-import Numeric (showHex)
-import Pipes
-import qualified Pipes.Prelude as P
+
+import Lens.Micro
+-- import GHC.Generics (Generic)
+-- import Numeric (showHex)
+-- import Pipes
+-- import qualified Pipes.Prelude as P
 import Text.Megaparsec.Pos
+
+-- import Lens.Micro.TH
+import Types
 
 pos1 :: Pos
 pos1 = unsafePos 1
 
-data TokenData
-  = TkVarId {-# UNPACK #-}!T.Text
-  | TkConId {-# UNPACK #-}!T.Text
-  | TkInt {-# UNPACK #-}!Int
-  | TkFloat {-# UNPACK #-}!Float
-  | TkChar {-# UNPACK #-}!Char
-  | TkString !String
-  | TkOther !T.Text
-  deriving (Read, Show, Eq, Generic)
-
-instance NFData TokenData
-
-data TokenPos = TokenPos
-  { _tokenPosPath :: !FilePath
-  , _tokenPosRows :: {-# UNPACK #-}!(Pos, Pos)
-  , _tokenPosCols :: {-# UNPACK #-}!(Pos, Pos)
-  } deriving (Read, Eq, Generic)
-
-instance NFData TokenPos
-
-instance Show TokenPos where
-  show (TokenPos path rows cols) =
-    show path ++ ":" ++ show_ rows ++ ":" ++ show_ cols
-    where
-      show_ (a, b) = "[" ++ show (unPos a) ++ ", " ++ show (unPos b) ++ ")"
-
-data Token =
-  Token !TokenPos
-        !TokenData
-  deriving (Read, Show, Eq, Generic)
-
-instance NFData Token
-
-getPosAfter :: Token -> (Pos, Pos)
-getPosAfter (Token (TokenPos _ r c) _) = (snd r, snd c)
-
--- |
-constEOT :: Char
-constEOT = Char.chr 0x03
-
-textStream :: Monad m => T.Text -> Producer Char m ()
-textStream = mapM_ yield . T.unpack >=> const (yield (Char.chr 0x03))
-
-invalidEOT :: Monad m => ExceptT String m a
-invalidEOT = throwE $ "Syntax error: Invalid character: EOT (\\0x02)."
-
--- | Given a row, column, and space-like character 'scanSpace' consumes a single
--- space/newline and returns the next @(row, column, char)@.
-scanSpace :: Monad m => Pos -> Pos -> Char -> Consumer' Char m (Pos, Pos, Char)
-scanSpace row col c
+scanSpace :: Char -> Input -> Input
+scanSpace c s
   | Char.isSpace c =
     case c of
-      '\t' -> await >>= tab
-      '\v' -> await >>= vtab
-      '\n' -> await >>= newline
-      '\f' -> await >>= newline
+      '\t' -> tab
+      '\v' -> vtab
+      '\n' -> newline
+      '\f' -> newline
       '\r' ->
-        await >>= \c' ->
-          if c' == '\n'
-            then await >>= newline
-            else newline c'
-      _ -> await >>= space
+        case s ^. text of
+          ('\n':cs) -> newline & text .~ cs
+          _ -> newline
+      _ -> space
   | otherwise =
     error "Panic!!! Lexer.scanSpace: received a non-space character."
   where
-    space c' = return $! (row, col <> pos1, c')
-    tab c' = return $! (row, col <> defaultTabWidth, c')
-    vtab c' = return $! (row <> pos1 <> pos1, pos1, c')
-    newline c' = return $! (row <> pos1, pos1, c')
+    space = s & (position . column) %~ (<> pos1)
+    tab = s & (position . column) %~ (<> defaultTabWidth)
+    vtab =
+      s & (position . row) %~ (<> pos1 <> pos1) & (position . column) .~ pos1
+    newline = s & (position . row) %~ (<> pos1) & (position . column) .~ pos1
 
--- scanComment :: Monad m => SourcePos -> Char -> Consumer' Char m (SourcePos, Char)
--- scanComment pos c
---   | c == constEOT =
-scanImpl ::
-     Monad m => Pos -> Pos -> Char -> Pipe Char Token (ExceptT String m) ()
-scanImpl row col c
-  | Char.isSpace c =
-    scanSpace row col c >>= \(row', col', c') -> scanImpl row' col' c'
-  -- By construction, input ends with 'Lexer.eofChar'. If, however, we encounter
-  -- 'Lexer.eofChar' while processing user input, it's a syntax error.
-  | c == constEOT = await >> lift invalidEOT
-  | otherwise = do
-    (c', token) <- nextToken "" row col c
-    yield token
-    let (row', col') = getPosAfter token
-    scanImpl row' col' c'
+scanImpl :: Char -> Input -> Except String [Token]
+scanImpl c s
+  | Char.isSpace c = scan $ scanSpace c s
+  | otherwise = scanToken c s >>= \(token, s') -> (:) token <$> scan s'
 
-scan :: Monad m => Pipe Char Token (ExceptT String m) ()
-scan = await >>= scanImpl pos1 pos1
+scan :: Input -> Except String [Token]
+scan s =
+  case s ^. text of
+    (c:cs) -> scanImpl c (s & text .~ cs)
+    [] -> return []
 
 scanSimple :: T.Text -> Either String [Token]
-scanSimple s = runIdentity . runExceptT . P.toListM $ scan <-< textStream s
+scanSimple = runExcept . scan . Input (SourcePos "" pos1 pos1) . T.unpack
 
---------------------------------------------------------------------------------
-lexUnknownImpl ::
-     Monad m => Pos -> Consumer' Char (ExceptT String m) (Pos, Char, String)
-lexUnknownImpl col =
-  await >>= \c ->
-    if Char.isSpace c || c == constEOT
-      then return (col, c, [])
-      else lexUnknownImpl (col <> pos1) >>= \(col', t, cs) ->
-             return (col', t, c : cs)
+step :: Input -> Input
+step = (position . column) %~ (<> pos1)
 
-lexUnknown ::
-     Monad m
-  => FilePath
-  -> Pos
-  -> Pos
-  -> Char
-  -> Consumer' Char (ExceptT String m) (Char, Token)
-lexUnknown path row col c =
-  lexUnknownImpl col >>= \(col', t, cs) ->
-    return $!
-    ( t
-    , Token (TokenPos path (row, row) (col, col')) . TkOther . T.pack $ c : cs)
+scanUnknown :: Char -> Input -> Except e (Token, Input)
+scanUnknown ch s =
+  let p = s ^. position . path
+      r = s ^. position . row
+      c = s ^. position . column
+      !token = Token (TokenPos p (r, r <> pos1) (c, c <> pos1)) (TkOther ch)
+  in return (token, step s)
 
---------------------------------------------------------------------------------
-lexVarIdImpl ::
-     Monad m => Pos -> Consumer' Char (ExceptT String m) (Pos, Char, String)
-lexVarIdImpl col =
-  await >>= \c ->
-    if Char.isAlphaNum c
-      then lexVarIdImpl (col <> pos1) >>= \(col', t, cs) ->
-             return (col', t, c : cs)
-      else return (col, c, [])
+scanIdImpl :: Input -> Except String (String, Input)
+scanIdImpl s
+  | null (s ^. text) = return ("", s)
+  | otherwise =
+    let (c:_) = s ^. text
+    in if Char.isAlphaNum c
+         then scanIdImpl (s & step & text %~ tail) >>= \(cs, s') ->
+                return (c : cs, s')
+         else return ("", s)
 
-lexVarId ::
-     Monad m
-  => FilePath
-  -> Pos
-  -> Pos
-  -> Char
-  -> Consumer' Char (ExceptT String m) (Char, Token)
-lexVarId path row col c =
-  lexVarIdImpl (col <> pos1) >>= \(col', t, cs) ->
-    return $!
-    ( t
-    , Token (TokenPos path (row, row) (col, col')) . TkVarId . T.pack $ c : cs)
+scanId :: Char -> Input -> Except String (TokenPos, T.Text, Input)
+scanId x s =
+  scanIdImpl (step s) >>= \(xs, s') ->
+    let p = s ^. position . path
+        r = s ^. position . row
+        r' = s' ^. position . row
+        c = s ^. position . column
+        c' = s' ^. position . column
+    in return (TokenPos p (r, r') (c, c'), T.pack $ x : xs, s')
 
---------------------------------------------------------------------------------
-{-
-lexConIdImpl :: Monad m => Consumer' Char (ExceptT String m) (Char, String)
-lexConIdImpl =
-  await >>= \c ->
-    if Char.isSpace c || c == eofChar
-      then return (c, [])
-      else if Char.isAlphaNum c
-             then lexConIdImpl >>= \(t, cs) -> return (t, c : cs)
-             else lift . throwE $
-                  "Syntax error: unexpected character: " ++ show c
+scanVarId :: Char -> Input -> Except String (Token, Input)
+scanVarId x s =
+  scanId x s >>= \(pos, txt, s') -> return (Token pos (TkVarId txt), s')
 
-lexConId :: Monad m => Char -> Consumer' Char (ExceptT String m) (Char, Token)
-lexConId c =
-  lexConIdImpl >>= \(t, cs) -> return (t, TokenConId . T.pack $ c : cs)
--}
---------------------------------------------------------------------------------
-nextToken ::
-     Monad m
-  => FilePath
-  -> Pos
-  -> Pos
-  -> Char
-  -> Consumer' Char (ExceptT String m) (Char, Token)
-nextToken path row col c =
-  case Char.generalCategory c of
-    Char.LowercaseLetter -> lexVarId path row col c
-    _ -> lexUnknown path row col c
+scanConId :: Char -> Input -> Except String (Token, Input)
+scanConId x s =
+  scanId x s >>= \(pos, txt, s') -> return (Token pos (TkConId txt), s')
+
+scanNum :: Char -> Input -> Except String (Token, Input)
+scanNum = scanUnknown
+
+scanToken :: Char -> Input -> Except String (Token, Input)
+scanToken x s =
+  case Char.generalCategory x of
+    Char.LowercaseLetter -> scanVarId x s
+    Char.UppercaseLetter -> scanConId x s
+    -- Char.TitlecaseLetter -> fail
+    -- Char.ModifierLetter -> fail
+    -- Char.OtherLetter -> fail
+    -- Char.NonSpacingMark -> fail
+    -- Char.SpacingCombiningMark -> fail
+    -- Char.EnclosingMark -> fail
+    Char.DecimalNumber -> scanNum x s
+    -- Char.LetterNumber -> fail
+    -- Char.OtherLetter -> fail
+    -- Char.ConnectorPunctuation -> fail
+    -- Char.DashPunctuation -> fail
+    -- Char.OpenPunctuation -> check for brackets
+    -- Char.ClosePunctuation -> check for brackets
+    -- Char.InitialQuote -> check for string
+    -- Char.FinalQuote -> check for string
+    -- Char.OtherPunctuation -> fail
+    -- Char.MathSymbol -> fail
+    -- Char.CurrencySymbol -> fail
+    -- Char.ModifierSymbol -> fail
+    -- Char.OtherSymbol -> fail
+    -- Char.Space -> fail
+    -- Char.LineSeparator -> fail
+    -- Char.ParagraphSeparator -> fail
+    -- Char.Control -> fail
+    -- Char.Format -> fail
+    -- Char.Surrogate -> fail
+    -- Char.PrivateUse -> fail
+    -- Char.NotAssigned -> fail
+    _ -> scanUnknown x s
 
 main :: IO ()
 main =
